c61c0140f6cf831e7416847732eff3b64a8fea23
姜.同.学 <806189218@qq.com>
2022-08-24 16:42:49 +0800

    2

--- main.py
+++ main.py
@@ -1,55 +1,90 @@
-from datetime import date, datetime
+from datetime import date, datetime, time
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
+import json
 import os
 import random
 
-today = datetime.now()
-start_date = '2021-12-10'
-city = '北京'
-birthday = '10-03'
 
-app_id = 'wx4267b99c0a9e982a'
-app_secret = '0f9e9b7bbaeb1ef4a006dabcefc7f638'
+class main:
+    #   初始化
+    #  参数：
+    #   微信公众号appid
+    #   微信公众号secret
+    #   开始时间start_date
+    #   城市city
+    #   生日birthday
+    #   用户id:user_id
+    #  模板id:template_id
+    def __init__(self, app_id, app_secret, start_date, city, birthday, user_id, template_id):
+        self.today = datetime.now()
+        self.start_date = start_date
+        self.city = city
+        self.birthday = birthday
+        self.app_id = app_id
+        self.app_secret = app_secret
+        self.user_id = user_id
+        self.template_id = template_id
+        print(self.template_id, self.user_id ,self.app_id, self.app_secret)
+        self.client = WeChatClient(self.app_id, self.app_secret)
 
-user_id = os.environ["USER_ID"]
-template_id = 'qC7E-QvdBZQmtZZOIQoqyxtQSkMpAC9HRdG-jZA_H2A'
+        self.wm = WeChatMessage(self.client)
+        self.wea, self.temperature, self.pm = self.get_weather()
+        self.data = {"weather": {"value": self.wea, "color": self.get_random_color()},
+                     "yan": {"value": self.get_yan(), "color": self.get_random_color()},
+                     "date": {"value": self.get_date(), "color": self.get_random_color()},
+                     "pm": {"value": self.pm, "color": self.get_random_color()},
+                     "temperature": {"value": self.temperature, "color": self.get_random_color()},
+                     "love_days": {"value": self.get_count(), "color": self.get_random_color()},
+                     "birthday_left": {"value": self.get_birthday(), "color": self.get_random_color()},
+                     "words": {"value": self.get_words()}}
+        self.res = self.wm.send_template(self.user_id, self.template_id, self.data)
+        print(self.res)
 
-print(city+app_id+app_secret+user_id)
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    def get_weather(self):
+        url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + self.city
+        res = requests.get(url).json()
+        weather = res['data']['list'][0]
+        return weather['weather'], math.floor(weather['temp']), math.floor(weather['pm25'])
 
-def get_count():
-  print(start_date)
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    #   获取今天日期及星期
+    def get_date(self):
+        return self.today.strftime("%Y-%m-%d") + " " + self.today.strftime("%A")
 
-def get_birthday():
-  print(birthday)
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    # 获取每日一言
+    def get_yan(self):
+        yan = requests.get("https://v.api.aa1.cn/api/yiyan/index.php").text
+        return yan[3:len(yan) - 4]
 
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    def get_count(self):
+        print(self.start_date)
+        delta = self.today - datetime.strptime(self.start_date, "%Y-%m-%d")
+        return delta.days
 
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    def get_birthday(self):
+        print(self.birthday)
+        next = datetime.strptime(str(date.today().year) + "-" + self.birthday, "%Y-%m-%d")
+        if next < datetime.now():
+            next = next.replace(year=next.year + 1)
+        return (next - self.today).days
+
+    def get_words(self):
+        words = requests.get("https://api.shadiao.pro/chp")
+        if words.status_code != 200:
+            return self.get_words()
+        return words.json()['data']['text']
+
+    def get_random_color(self):
+        return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
-client = WeChatClient(app_id, app_secret)
-
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+if __name__ == '__main__':
+    # list = '[{"app_id": "wx4267b99c0a9e982a","app_secret": "0f9e9b7bbaeb1ef4a006dabcefc7f638","start_date": "2021-12-10","city": "北京","birthday": "10-03","user_id": "ouipF6gVu2Qwt2wCH27nSnVwS6mE","template_id": "jdI2j7APhfAvmSrCwCs_BI-att9xk0bYO2QwdykQhys"}]'
+    list =  os.environ["list"]
+    list = json.loads(list)
+    for i in list:
+        main(i['app_id'], i['app_secret'], i['start_date'], i['city'], i['birthday'], i['user_id'], i['template_id'])
+        print("第" + str(list.index(i) + 1) + "个发送成功")
+    print("完成")
